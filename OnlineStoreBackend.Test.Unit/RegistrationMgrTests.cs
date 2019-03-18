0c130758a4d89da1f14bd2c5a833da4c4fae7a02
DennyHsieh <tc.hsiehhsieh@gmail.com>
2019-03-18 15:44:28 +0800

    finsih testing username

--- OnlineStoreBackend.Test.Unit/RegistrationMgrTests.cs
+++ OnlineStoreBackend.Test.Unit/RegistrationMgrTests.cs
@@ -10,17 +10,49 @@ namespace OnlineStoreBackend.Test.Unit
     [TestFixture]
     public class RegistrationMgrTests
     {
-        [Test]
-        public void IsValidUsername_ValidUsername_ReturnsTrue()
+        [TestCase("usr")]
+        [TestCase("user123")]
+        [TestCase("chengwei45")]
+        [TestCase("123test456")]
+        public void IsValidUsername_ValidUsername_ReturnsTrue(string username)
         {
             // Arrange
             RegistrationMgr regMgr = new RegistrationMgr();
 
             // Act
-            bool isValid = regMgr.IsValidUsername("user");
+            bool isValid = regMgr.IsValidUsername(username);
 
             // Assert
             Assert.True(isValid);
+        }
+
+        [TestCase("u")]
+        [TestCase("us")]
+        [TestCase("user5678901")]
+        public void IsValidUsername_InvalidUsernameLength_ReturnsFalse(string username)
+        {
+            // Arrange
+            RegistrationMgr regMgr = new RegistrationMgr();
+
+            // Act
+            bool isValid = regMgr.IsValidUsername(username);
+
+            // Assert
+            Assert.False(isValid);
+        }
+
+        [TestCase("“user@123")]
+        [TestCase("!user")]
+        public void IsValidUsername_InvalidUsernameChars_ReturnsFalse(string username)
+        {
+            // Arrange
+            RegistrationMgr regMgr = new RegistrationMgr();
+
+            // Act
+            bool isValid = regMgr.IsValidUsername(username);
+
+            // Assert
+            Assert.False(isValid);
         }
     }
 }
--- OnlineStoreBackend/RegistrationMgr.cs
+++ OnlineStoreBackend/RegistrationMgr.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace OnlineStoreBackend
 {
     public class RegistrationMgr
     {
-        public bool IsValidUsername(string v)
+       
+        public bool IsValidUsername(string username)
         {
-            // throw new NotImplementedException();
+            // Length must between [3, 10]
+            if (username.Length < 3 || username.Length >10)
+            {
+                return false;
+            }
+
+            // Chars must be alphanumeric
+            Regex r = new Regex("^[a-zA-Z0-9]+$");
+            if (!r.IsMatch(username))
+            {
+                return false;
+            }
             return true;
         }
     }
