0ba906538df0a7e72763b686ef2460b2a16f3839
DennyHsieh <tc.hsiehhsieh@gmail.com>
2019-03-18 15:26:27 +0800

    first commit

---
+++ OnlineStoreBackend.Test.Unit/RegistrationMgrTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreBackend.Test.Unit
+{
+    [TestFixture]
+    public class RegistrationMgrTests
+    {
+        [Test]
+        public void IsValidUsername_ValidUsername_ReturnsTrue()
+        {
+            // Arrange
+            RegistrationMgr regMgr = new RegistrationMgr();
+
+            // Act
+            bool isValid = regMgr.IsValidUsername("user");
+
+            // Assert
+            Assert.True(isValid);
+        }
+    }
+}
---
+++ OnlineStoreBackend/RegistrationMgr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStoreBackend
+{
+    public class RegistrationMgr
+    {
+        public bool IsValidUsername(string v)
+        {
+            // throw new NotImplementedException();
+            return true;
+        }
+    }
+}
